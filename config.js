16b8abb2939e948dfd9f286863915176f01f217d
Devil-king-Bots <156557429+Devil-king-Bots@users.noreply.github.com>
2024-02-06 21:00:11 +0500

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,15 @@ module.exports = {
   KOYEB_API: process.env.KOYEB_API || '',
   SERVERURL: process.env.SERVERURL || '',
   SERVER: process.env.SERVER || 'defeat',
-  ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
+  ALIVE_DATA : process.env.ALIVE_DATA || "_*Hi im😈 DEVIL-KING-BOT😈
+
+ALIVE  BOT
+
+👾OWNER NUMBER 👾  
+
+Wa.me/923014283429
+
+Creat by DEVIL-KING-BOT: &platform_\n_RUNTIME : &update your alive message_",
   IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
   SESSION_ID: process.env.SESSION_ID || 'ZnNneU5_J_A_R_V_I_S_ZcTU=',
   LANG: process.env.LANG || 'EN',
@@ -31,12 +39,12 @@ module.exports = {
   AUTO_STATUS_VIEWS: toBool(process.env.AUTO_STATUS_VIEWS || 'false'),
   WELCOME_MSG: process.env.WELCOME_MSG || 'Hi @user Welcome to @gname',
   GOODBYE_MSG: process.env.GOODBYE_MSG || 'Hi @user It was Nice Seeing you',
-  AUTHOR: process.env.AUTHOR || 'ʟᴏᴋɪ-xᴇʀ',
+  AUTHOR: process.env.AUTHOR || 'Devil-king',
   MEDIA_DATA: process.env.MEDIA_DATA|| 'ʟɪꜱᴛ ᴍᴇɴᴜ;ᴊᴀʀᴠɪꜱ-ᴍᴅ;https://i.imgur.com/xzIo1nd.jpeg',
   SUDO: process.env.SUDO || '917025673121,',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || '',
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || '',
   BOT_INFO: process.env.BOT_INFO || 'ᴊᴀʀᴠɪꜱ;ʟᴏᴋɪ-xᴇʀ;https://i.imgur.com/xzIo1nd.jpeg',
-  WORK_TYPE: process.env.WORK_TYPE || 'private',
+  WORK_TYPE: process.env.WORK_TYPE || 'pubilc',
   DATABASE: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: "postgres", ssl: true, protocol: "postgres", dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
 };
